801b4b10e28ac843ef89383e7f77f6e063451614
Stephen Wong <wongstephenky@gmail.com>
2022-12-10 16:52:27 -0800

    adjusted odd number of events

--- src/components/Events.js
+++ src/components/Events.js
@@ -21,24 +21,29 @@ export const Events = () => {
             News and Events
           </h3>
         </div>{" "}
-        <div className="flex flex-wrap w-full pt-12 -mx-4 justify-left">
+        <div className="flex flex-wrap w-full pt-12 -mx-4 justify-lef ">
           {posts ? (
             posts.data.map((post, idx) => (
-              <div className="w-1/2 px-4 mb-4 max-w-prose" key={idx}>
+              <div
+                className={`w-1/2 px-4 mb-4 ${
+                  posts.data.length % 2 !== 0 && "first:w-full"
+                }`}
+                key={idx}
+              >
                 <div className="h-full group">
                   <div className="relative h-full px-8 pt-16 pb-8 transition duration-200 bg-white rounded-sm shadow-sm hover:shadow-md">
-                    <h3 className="mb-4 text-xl font-bold leading-7 text-gray-900">
+                    <h3 className="mb-4 text-xl font-bold leading-7 text-gray-900 md:text-2xl">
                       {post.attributes.Title}
                     </h3>
                     <ReactMarkdown
-                      className="mb-12 font-medium text-gray-600 transition duration-200 group-hover:text-gray-700"
+                      className="mb-12 font-medium tracking-wider text-gray-600 transition duration-200 group-hover:text-gray-700"
                       rehypePlugins={[rehypeRaw]}
                       children={post.attributes.Body}
                       linkTarget={"_blank"}
                     />
                     {post.attributes.Media.data && (
                       <img
-                        className="mx-auto"
+                        className="w-full h-auto max-w-xs mx-auto"
                         src={
                           process.env.REACT_APP_HOST +
                           post.attributes.Media.data.attributes.formats.small
